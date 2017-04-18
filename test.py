07ed76f3f1682e05d68cb4ac45714a09f3db70d8
Mike Sanders <michael.j.sanders@gmail.com>
2017-04-18 17:02:28 -0400

    mike again

--- test.py
+++ test.py
@@ -3,5 +3,8 @@ print("hello world")
 import numpy as np
 import pandas as pd
 
-test_set = pd.read_csv('/Users/michaelsanders/datasets/Project4/test.csv')
-test_set.columns
+train_set = pd.read_csv('/Users/michaelsanders/datasets/Project4/train.csv')
+train_set.columns
+train_set.dtypes
+
+train_set.AddressAccuracy.head()
