1fc88d5e523d54a682c0d905e6728339dc7289f1
Mike Sanders <michael.j.sanders@gmail.com>
2017-04-19 12:16:35 -0400

    very initial EDA script and notes doc

---
+++ code/MikeEDA.py
@@ -0,0 +1,81 @@
+#!bin/bash/python
+
+import numpy as np
+import pandas as pd
+
+
+train_set = pd.read_csv('../data/input/train.csv')
+train_set.columns
+train_set.dtypes
+
+train_set.AddressAccuracy.head()
+train_set.Date.head()
+train_set.Date = pd.to_datetime(train_set.Date)
+
+train_set.Address.head()
+
+train_set.WnvPresent.value_counts()
+
+train_set.groupby([train_set.Date.year, train_set.Date.month]).WnvPresent.value_counts()
+
+WNV_by_month_year = train_set.groupby(by=[train_set.Date.map(lambda x : (x.year, x.month))]).WnvPresent.value_counts()
+
+WNV_by_year = train_set.groupby(by=[train_set.Date.map(lambda x : (x.year))]).WnvPresent.value_counts()
+
+
+
+
+
+weather = pd.read_csv('../data/input/weather.csv')
+weather.CodeSum.value_counts()
+expanded_codes = weather['CodeSum'].str.split(' ', expand=True)
+
+
+
+weather_types = {
+'+FC': 'TORNADO/WATERSPOUT',
+'FC': 'FUNNEL CLOUD',
+'TS': 'THUNDERSTORM',
+'GR': 'HAIL',
+'RA': 'RAIN',
+'DZ': 'DRIZZLE',
+'SN': 'SNOW',
+'SG': 'SNOW GRAINS',
+'GS': 'SMALL HAIL ANDOR SNOW PELLETS',
+'PL': 'ICE PELLETS',
+'IC': 'ICE CRYSTALS',
+'FG+': 'HEAVY FOG',
+'FG': 'FOG',
+'BR': 'MIST',
+'UP': "UNKNOWN PRECIPITATION",
+'HZ': 'HAZE',
+'FU': 'SMOKE',
+'VA': 'VOLCANIC ASH',
+'DU': 'WIDESPREAD DUST',
+'DS': 'DUSTSTORM',
+'PO': 'SAND_DUST WHIRLS',
+'SA': 'SAND',
+'SS': 'SANDSTORM',
+'PY': 'SPRAY',
+'SQ': 'SQUALL',
+'DR': 'LOW DRIFTING',
+'SH': 'SHOWER',
+'FZ': 'FREEZING',
+'MI': 'SHALLOW',
+'PR': 'PARTIAL',
+'BC': 'PATCHES',
+'BL': 'BLOWING',
+'VC': 'VICINITY'}
+
+
+weather_cols = []
+for key in weather_types:
+    weather_cols.append(weather_types[key])
+
+weather = pd.concat([weather,pd.DataFrame(columns=weather_cols)])
+
+def dummy_weather():
+    for 
+
+
+
